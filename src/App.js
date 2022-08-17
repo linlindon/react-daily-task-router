088e46658b253df0c9ebbf9f48f4a83881514c3a
Yiling Lin <a31420o@gmail.com>
2022-08-17 14:09:19 +0800

    add routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
-import './App.css';
-import {
-  HashRouter,
-  NavLink,
-} from 'react-router-dom';
+import "./App.css";
+import { HashRouter, NavLink, Route, Routes } from "react-router-dom";
 
 const Todo = () => {
-  return <p>這是 Todo 頁面 
-  </p>;
+  return <p>這是 Todo 頁面</p>;
 };
 const Login = () => {
   return <p>這是登入頁面</p>;
@@ -34,6 +30,12 @@ function App() {
           </NavLink>
         </div>
         {/* Routes, Route 練習區 */}
+        <Routes>
+          <Route path="/" />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/todo" element={<Todo />} />
+        </Routes>
         {/* 練習區 */}
       </HashRouter>
     </div>
